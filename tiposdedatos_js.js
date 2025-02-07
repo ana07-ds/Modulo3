f590b77e5d81634be87b4926e9c738a9b8c56184
Ana Diaz <anadiazsanchez7@gmail.com>
2025-02-06 19:18:33 -0600

    tipos de datos

---
+++ tiposdedatos_js.js
@@ -0,0 +1,10 @@
+console.log(typeof 42); //number
+console.log(typeof 'Veinticinco'); //string
+console.log(typeof -666); //number
+console.log(typeof true); //boolean
+console.log(typeof 0); //number
+console.log(typeof ''); //String
+console.log(typeof null); //null
+console.log(typeof undefined); //undefined
+console.log(typeof FALSE); //undefined
+console.log(typeof "123"); //String
